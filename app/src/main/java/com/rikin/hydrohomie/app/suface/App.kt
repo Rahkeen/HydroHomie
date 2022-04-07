371fa4bb9bea04dd35dca53f2a4258432eb7e592
Rikin Marfatia <rikinm10@gmail.com>
2022-04-06 22:05:49 -0700

    move stuff into App Composable

--- app/src/main/java/com/rikin/hydrohomie/app/suface/App.kt
+++ app/src/main/java/com/rikin/hydrohomie/app/suface/App.kt
@@ -1,34 +1,48 @@
 package com.rikin.hydrohomie.app.suface
 
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.airbnb.mvrx.compose.collectAsState
 import com.airbnb.mvrx.compose.mavericksActivityViewModel
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.rikin.hydrohomie.app.domain.AppViewModel
+import com.rikin.hydrohomie.design.HydroHomieTheme
 import com.rikin.hydrohomie.features.hydration.surface.Hydration
 import com.rikin.hydrohomie.features.streak.surface.Streaks
-import logcat.logcat
 
 @Composable
 fun App() {
-  val navController = rememberNavController()
+  val systemUiController = rememberSystemUiController()
+  val useDarkIcons = MaterialTheme.colors.isLight
 
-  NavHost(navController = navController, startDestination = "hydration") {
-    composable("hydration") {
-      val viewModel: AppViewModel = mavericksActivityViewModel()
-      val state by viewModel.collectAsState { it.currentHydration }
-      logcat { "Current State: $state" }
-      Hydration(state = state, actions = viewModel::send, navigation = navController::navigate)
-    }
+  SideEffect {
+    systemUiController.setSystemBarsColor(
+      color = Color.Transparent,
+      darkIcons = useDarkIcons
+    )
+  }
 
-    composable("streaks") {
-      val viewModel: AppViewModel = mavericksActivityViewModel()
-      val state by viewModel.collectAsState { it.streakState }
-      logcat { "Current State: $state" }
-      Streaks(state = state)
+  HydroHomieTheme {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "hydration") {
+      composable("hydration") {
+        val viewModel: AppViewModel = mavericksActivityViewModel()
+        val state by viewModel.collectAsState { it.currentHydration }
+        Hydration(state = state, actions = viewModel::send, navigation = navController::navigate)
+      }
+
+      composable("streaks") {
+        val viewModel: AppViewModel = mavericksActivityViewModel()
+        val state by viewModel.collectAsState { it.streakState }
+        Streaks(state = state)
+      }
     }
   }
 }
--- app/src/main/java/com/rikin/hydrohomie/app/suface/MainActivity.kt
+++ app/src/main/java/com/rikin/hydrohomie/app/suface/MainActivity.kt
@@ -3,12 +3,7 @@ package com.rikin.hydrohomie.app.suface
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
 import androidx.core.view.WindowCompat
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
-import com.rikin.hydrohomie.design.HydroHomieTheme
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,19 +12,7 @@ class MainActivity : ComponentActivity() {
     WindowCompat.setDecorFitsSystemWindows(window, false)
 
     setContent {
-      val systemUiController = rememberSystemUiController()
-      val useDarkIcons = MaterialTheme.colors.isLight
-
-      SideEffect {
-        systemUiController.setSystemBarsColor(
-          color = Color.Transparent,
-          darkIcons = useDarkIcons
-        )
-      }
-
-      HydroHomieTheme {
-        App()
-      }
+      App()
     }
   }
 }
